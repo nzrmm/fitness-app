56b04222bbceffb15b719058c47a4599b0b42ca4
nzrmm <iamgata.me@gmail.com>
2021-12-29 04:57:11 +0700

    add loading amination and beauty scrollbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,25 @@
 <template>
-  <MainLayouts>
+  <BasePageLoading v-if="isLoading" />
+  <MainLayouts v-else>
     <router-view></router-view>
   </MainLayouts>
 </template>
+
+<script>
+import { ref, onMounted } from 'vue';
+export default {
+  setup() {
+    const isLoading = ref(true);
+    onMounted(() => {
+      document.onreadystatechange = () => {
+        if (document.readyState == 'complete') {
+          isLoading.value = false;
+        }
+      };
+    });
+    return {
+      isLoading,
+    };
+  },
+};
+</script>
---
+++ src/components/base/BasePageLoading.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="w-full h-screen elcenter bg-black text-white">
+    <div class="elcenter flex-col">
+      <h1
+        class="mb-6 animate-bounce uppercase text-6xl font-bold tracking-wider">
+        Workout
+      </h1>
+      <IconLoading />
+    </div>
+  </div>
+</template>
