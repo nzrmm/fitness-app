bfc5d2105b9f82c401cdd5a87adc3aba774d21e3
nzrmm <iamgata.me@gmail.com>
2021-12-29 06:05:53 +0700

    [fix] not supported in mobile device

--- src/components/layouts/MainLayouts.vue
+++ src/components/layouts/MainLayouts.vue
@@ -1,8 +1,41 @@
 <template>
   <div class="antialiased min-h-screen bg-black text-white overflow-hidden">
-    <div class="container">
+    <div v-if="!isMobile" class="container">
       <TheNavbar />
       <slot></slot>
     </div>
+    <div v-else class="px-6 elcenter flex-col text-center w-full h-screen">
+      <span class="mb-6 text-8xl">🙁</span>
+      <h2 class="mb-4 text-2xl font-semibold">
+        Sorry, not supported on mobile devices.
+      </h2>
+      <span class="text-sm text-white/60">
+        To visit the site, please open with desktop only !
+      </span>
+    </div>
   </div>
 </template>
+
+<script>
+import { ref } from 'vue';
+export default {
+  setup() {
+    const isMobile = ref();
+    const checkScreen = () => {
+      const windowWidth = window.innerWidth;
+      if (windowWidth <= 1024) {
+        isMobile.value = true;
+        return;
+      }
+      isMobile.value = false;
+    };
+
+    checkScreen();
+    window.addEventListener('resize', checkScreen);
+
+    return {
+      isMobile,
+    };
+  },
+};
+</script>
